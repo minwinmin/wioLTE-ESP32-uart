e38147aee7664b9b3a0a2773c99363ca492da575
Takuro Mikami <takuro.mikami@gmail.com>
2020-05-21 14:55:30 +0900

    new

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,12 +1,25 @@
+#include <DHT.h>
+const int PIN_DHT = 4;
+
 String sendstr = "esp32";
+DHT dht(PIN_DHT, DHT11); 
 
 void setup(){
 //  Serial.begin(115200);
   Serial.begin(57600);
   Serial.println("Start writeing.");
+  dht.begin();
 }
 
 void loop(){
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Temperature:");
+  Serial.print(temperature);
+  Serial.print("*C");
+  
   for (int i = 0; i < sendstr.length(); i++){
     Serial.write(sendstr.charAt(i)); 
   }
